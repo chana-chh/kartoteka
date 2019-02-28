3cf7d3623c4d4737caddea28c47787e96bdbe62b
chana-chh <chanasrb@gmail.com>
2019-02-28 20:47:45 +0100

    login redirect

--- app/middlewares/AuthMiddleware.php
+++ app/middlewares/AuthMiddleware.php
@@ -10,6 +10,8 @@ class AuthMiddleware extends Middleware
     public function __invoke($request, $response, $next)
     {
         if (!$this->auth->isLoggedIn()) {
+            $url = (string)$request->getUri();
+            $_SESSION['LOGIN_URL'] = $url;
             $this->flash->addMessage('warning', 'Samo za prijavljene korisnike');
             return $response->withRedirect($this->router->pathFor('prijava'));
         }
